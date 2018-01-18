240c115fd38832d6690e94f984824af7cc44a060
Samantha Bajis <Sammi.Bajis@gmail.com>
2018-01-17 23:57:33 -0500

    First Commit
    Capstones first commit to GitHub page.

---
+++ "I'll be Home for Christmas"/Assets/Collactable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Collactable : MonoBehaviour 
+{
+	public GameObject Enable_Disable;
+
+	public int value;
+
+	public void Enable()
+	{
+		Enable_Disable.SetActive (true);
+	}
+	public void Disable()
+	{
+		Enable_Disable.SetActive (false);
+		GameManager.instance.Collect (value, gameObject); 
+	}
+}
---
+++ "I'll be Home for Christmas"/Assets/changeColour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColour : MonoBehaviour {
+	public Color startColour;
+	public Color mouseOverColour;
+	bool mouseOver = false;
+
+	public void OnMouseEnter(){
+		mouseOver = true;
+		GetComponent <Renderer> ().material.SetColor ("_Color", mouseOverColour);
+	}
+	public void OnMouseExit(){
+		mouseOver = false;
+		GetComponent<Renderer> ().material.SetColor ("_Color", startColour);
+	}
+}
+
